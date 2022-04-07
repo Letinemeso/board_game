9e354fc510a23a3d5095fda5a636d0dd5b067a2d
a.hiryanov <a.hiryanov@octaviangameart.ru>
2022-04-07 18:02:52 +0300

    trying to create opponent AI

--- Main.cpp
+++ Main.cpp
@@ -9,47 +9,88 @@
 
 #include "Object.h"
 
+
+struct intp
+{
+    int x = 0, y = 0;
+    intp(int _x, int _y) : x(_x), y(_y) {}
+};
+
+intp start = {0, 0};
+intp dest = {2, 3};
+bool node[4][4] = {{false}};
+
 int main()
 {
-	LEti::Event_Controller::init_and_create_window(800, 600, "Board Game");
-
-	glEnable(GL_BLEND);
-	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-
-	LEti::Shader::init_shader("resources/shaders/vertex_shader.shader", "resources/shaders/fragment_shader.shader");
-	ASSERT(!LEti::Shader::is_valid());
-	LEti::Shader::set_texture_uniform_name("input_texture");
-	LEti::Shader::set_transform_matrix_uniform_name("transform_matrix");
-	LEti::Shader::set_projection_matrix_uniform_name("projection_matrix");
-
-	LEti::Camera::setup_orthographic_matrix();
+    node[0][1] = false;
+    node[0][2] = false;
+    node[1][1] = false;
+    node[1][3] = false;
+    node[2][2] = false;
 
 
-	LEti::Resource_Loader::load_object("bgr", "resources/models/background.mdl");
-	LEti::Resource_Loader::load_object("field", "resources/models/field.mdl");
-    LEti::Resource_Loader::load_object("pawn_cross", "resources/models/pawn_cross.mdl");
-    LEti::Resource_Loader::load_object("pawn_circle", "resources/models/pawn_circle.mdl");
 
-	LEti::Object background;
-	background.init("bgr");
 
-    Game_Controller controller("field", "pawn_cross", "pawn_circle");
 
-	while (!LEti::Event_Controller::window_should_close())
-	{
-		glClear(GL_COLOR_BUFFER_BIT);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//	LEti::Event_Controller::init_and_create_window(800, 600, "Board Game");
+
+//	glEnable(GL_BLEND);
+//	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+//	LEti::Shader::init_shader("resources/shaders/vertex_shader.shader", "resources/shaders/fragment_shader.shader");
+//	ASSERT(!LEti::Shader::is_valid());
+//	LEti::Shader::set_texture_uniform_name("input_texture");
+//	LEti::Shader::set_transform_matrix_uniform_name("transform_matrix");
+//	LEti::Shader::set_projection_matrix_uniform_name("projection_matrix");
+
+//	LEti::Camera::setup_orthographic_matrix();
+
+
+//	LEti::Resource_Loader::load_object("bgr", "resources/models/background.mdl");
+//	LEti::Resource_Loader::load_object("field", "resources/models/field.mdl");
+//    LEti::Resource_Loader::load_object("pawn_cross", "resources/models/pawn_cross.mdl");
+//    LEti::Resource_Loader::load_object("pawn_circle", "resources/models/pawn_circle.mdl");
+
+//	LEti::Object background;
+//	background.init("bgr");
+
+//    Game_Controller controller("field", "pawn_cross", "pawn_circle");
+
+//	while (!LEti::Event_Controller::window_should_close())
+//	{
+//		glClear(GL_COLOR_BUFFER_BIT);
 	
-		LEti::Event_Controller::update();
-		LEti::Camera::update(true, false);
+//		LEti::Event_Controller::update();
+//		LEti::Camera::update(true, false);
 
-        controller.update();
+//        controller.update();
 
-		background.draw();
+//		background.draw();
 
-        controller.draw();
+//        controller.draw();
 
-		LEti::Event_Controller::swap_buffers();
-	}
+//		LEti::Event_Controller::swap_buffers();
+//	}
 
 
 	return 0;
